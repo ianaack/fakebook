5445e99d560078e2e3eacb964a4f6a67ebc64195
ianaack <ianaack@gmail.com>
2022-06-15 19:50:00 -0400

    User Model Added

--- models/User.js
+++ models/User.js
@@ -1,2 +1,45 @@
 const { Schema, model } = require("mongoose");
-const dateFormat = require("../utils/dateFormat");
+
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			unique: true,
+			required: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			match: /.+\@.+\..+/,
+		},
+		thoughts: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "Thought",
+			},
+		],
+		friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ],
+	},
+	{
+		toJSON: {
+			virtuals: true,
+		},
+		id: false,
+	}
+);
+
+// get total count of friends on retrieval
+userSchema.virtual("friendCount").get(function () {
+	return this.friends.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
